3c5f24370d22b0c5a91941af8765509705a367bf
Swaroop Dhungana <dhunganaswaroop@gmail.com>
2023-01-25 21:17:00 +0545

    registration done

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
-import { FcGoogle } from "react-icons/fc"
 import LoginImg from "../assets/login/1.jpg"
 function Login() {
   return (
@@ -13,20 +12,12 @@ function Login() {
           Don't have an account yet?
           <Link
             to="/signup"
-            className="text-yellow-500 hover:text-yellow-300 hover:underline"
+            className="text-yellow-500 hover:text-yellow-300 hover:underline ml-1"
           >
             Sign up Free
           </Link>
         </p>
-        <button className="px-3 py-3 bg-white shadow-lg rounded-lg flex items-center gap-x-2 w-2/3 text-gray-500 justify-center">
-          <FcGoogle />
-          Log in with Google
-        </button>
-        <div className="flex justify-between items-center gap-x-4 w-2/3">
-          <hr className="w-full border-yellow-500" />
-          or
-          <hr className="w-full border-yellow-500" />
-        </div>
+
         <div className="flex flex-col w-full items-center justify-center gap-y-4">
           <input
             type="text"
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,9 +1,50 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import { FcGoogle } from "react-icons/fc"
 import LoginImg from "../assets/login/1.jpg"
-
+import Axios from "axios"
 function Signup() {
+  const [sendRequest, setSendRequest] = useState(false)
+  const [usernameValue, setUsernameValue] = useState("")
+  const [emailValue, setEmailValue] = useState("")
+  const [passwordValue, setPasswordValue] = useState("")
+  const [re_passwordValue, setRePasswordValue] = useState("")
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log("Form Submitted")
+    setSendRequest(!sendRequest)
+  }
+
+  useEffect(() => {
+    if (sendRequest) {
+      const source = Axios.CancelToken.source()
+      async function SignUp() {
+        try {
+          const response = await Axios.post(
+            "http://127.0.0.1:8000/api-auth-djoser/users/",
+            {
+              username: usernameValue,
+              email: emailValue,
+              password: passwordValue,
+              re_password: re_passwordValue,
+            },
+            {
+              cancelToken: source.token,
+            }
+          )
+          console.log(response)
+        } catch (error) {
+          console.log(error)
+        }
+      }
+      SignUp()
+      return () => {
+        source.cancel
+      }
+    }
+  }, [sendRequest])
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2">
       <div className="flex flex-col items-center w-full h-screen justify-center space-y-10 bg-gray-700 px-28 text-white">
@@ -12,49 +53,52 @@ function Signup() {
           Already have an account yet?
           <Link
             to="/login"
-            className="text-yellow-500 hover:text-yellow-300 hover:underline"
+            className="text-yellow-500 hover:text-yellow-300 hover:underline ml-1"
           >
             Login
           </Link>
         </p>
-        <button className="px-3 py-3 bg-white shadow-lg rounded-lg flex items-center gap-x-2 w-2/3 text-gray-500 justify-center">
-          <FcGoogle />
-          Login with Google
-        </button>
-        <div className="flex justify-between items-center gap-x-4 w-2/3">
-          <hr className="w-full border-yellow-500" />
-          or
-          <hr className="w-full border-yellow-500" />
-        </div>
 
-        <div className="flex flex-col w-full items-center justify-center gap-y-4">
-          <div className="w-2/3 flex gap-x-2">
-            <input
-              type="text"
-              placeholder="First Name"
-              className="px-3 py-3 w-1/2 rounded-lg text-black"
-            />
-            <input
-              type="text"
-              placeholder="Last Name"
-              className="px-3 py-3 w-1/2 rounded-lg text-black"
-            />
-          </div>
+        <form
+          onSubmit={submitHandler}
+          className="flex flex-col w-full items-center justify-center gap-y-4"
+        >
+          <input
+            type="text"
+            placeholder="Username"
+            className="px-3 py-3 w-2/3 rounded-lg text-black"
+            value={usernameValue}
+            onChange={(e) => setUsernameValue(e.target.value)}
+          />
           <input
             type="text"
             placeholder="name@email.com"
             className="px-3 py-3 w-2/3 rounded-lg text-black"
+            value={emailValue}
+            onChange={(e) => setEmailValue(e.target.value)}
           />
           <input
             type="password"
             placeholder="Password"
             className="px-3 py-3 w-2/3 rounded-lg text-black"
+            value={passwordValue}
+            onChange={(e) => setPasswordValue(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            className="px-3 py-3 w-2/3 rounded-lg text-black"
+            value={re_passwordValue}
+            onChange={(e) => setRePasswordValue(e.target.value)}
           />
 
-          <button className="px-3 py-3 bg-yellow-500 w-2/3 rounded-lg">
-            Login
+          <button
+            className="px-3 py-3 bg-yellow-500 w-2/3 rounded-lg mt-5"
+            type="submit"
+          >
+            Register
           </button>
-        </div>
+        </form>
       </div>
       <img
         src={LoginImg}
