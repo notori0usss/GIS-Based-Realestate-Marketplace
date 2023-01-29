a91706ac796f3d7329dca5b886449646686b1e1e
Swaroop Dhungana <dhunganaswaroop@gmail.com>
2023-01-29 16:47:52 +0545

    Draggable Marker

--- frontend/src/pages/AddProperty.jsx
+++ frontend/src/pages/AddProperty.jsx
@@ -1,10 +1,14 @@
-import React, { useEffect } from "react"
+import React, { useState, useEffect, useRef, useMemo } from "react"
 import Navbar from "../layout/Navbar"
 import { useImmerReducer } from "use-immer"
 import PropertyForm from "../components/PropertyForm"
 import Axios from "axios"
-import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet"
+import { MapContainer, TileLayer, useMap, Marker, Polygon } from "react-leaflet"
+import Kirtipur from "../data/GeoJSON/Kirtipur"
+import Balkot from "../data/GeoJSON/Balkot"
+import { AiFillDollarCircle } from "react-icons/ai"
 function AddProperty() {
+  //Initial States for Form
   const initialState = {
     titleValue: "",
     listingTypeValue: "",
@@ -29,8 +33,14 @@ function AddProperty() {
     picture4Value: "",
     picture5Value: "",
     mapInstance: null,
+    markerPosition: {
+      lat: 27.704111212111023,
+      lng: 85.31943175211019,
+    },
+    uploadedImages: [],
     sendRequest: 0,
   }
+  //Reducerfuntion
   function ReducerFunction(draft, action) {
     switch (action.type) {
       case "catchTitleChange":
@@ -103,17 +113,36 @@ function AddProperty() {
       case "getMap":
         draft.mapInstance = action.mapData
         break
+      case "changeMarkerPosition":
+        draft.markerPosition.lat = action.changeLatitude
+        draft.markerPosition.lng = action.changeLongitude
+        draft.latitudeValue = ""
+        draft.longitudeValue = ""
+        break
+      case "catchUploadedImages":
+        draft.uploadedImages = action.imagesChosen
       case "changeSendRequest":
         draft.sendRequest = draft.sendRequest + 1
         break
     }
   }
+  // getting map instance
   function MapComponent() {
     const map = useMap()
     dispatch({ type: "getMap", mapData: map })
     return null
   }
+  //displays polygon on the map
+  function areaDisplay() {
+    if (state.municipalityValue === "Kirtipur") {
+      return <Polygon positions={Kirtipur} />
+    } else if (state.municipalityValue === "Balkot") {
+      return <Polygon positions={Balkot} />
+    }
+  }
+  //using immerreducer
   const [state, dispatch] = useImmerReducer(ReducerFunction, initialState)
+  //form submit handler
   const submitHandler = (e) => {
     e.preventDefault()
     dispatch({ type: "sendRequest" })
@@ -150,20 +179,65 @@ function AddProperty() {
   //     }
   //   }
   // }, [state.sendRequest])
+
+  //for Changing the map position based on municipality
   useEffect(() => {
-    if (state.municipalityValue === "Siphal") {
-      state.mapInstance.flyTo([27.712637153159196, 85.3425513784727], 15)
+    if (state.municipalityValue === "Balkot") {
+      state.mapInstance.flyTo([27.666367937321187, 85.37419795932625], 13)
+      dispatch({
+        type: "changeMarkerPosition",
+        changeLatitude: 27.666367937321187,
+        changeLongitude: 85.37419795932625,
+      })
     } else if (state.municipalityValue === "Kirtipur") {
-      state.mapInstance.flyTo([27.67887766605217, 85.27391419425689], 15)
+      state.mapInstance.flyTo([27.67887766605217, 85.27391419425689], 13)
+      dispatch({
+        type: "changeMarkerPosition",
+        changeLatitude: 27.67887766605217,
+        changeLongitude: 85.27391419425689,
+      })
     }
   }, [state.municipalityValue])
+
+  //for changing map position based on area
   useEffect(() => {
     if (state.areaValue === "Kathmandu") {
-      state.mapInstance.flyTo([27.711964791049617, 85.32270046066495], 15)
+      state.mapInstance.flyTo([27.711964791049617, 85.32270046066495], 13)
+      dispatch({
+        type: "changeMarkerPosition",
+        changeLatitude: 27.711964791049617,
+        changeLongitude: 85.32270046066495,
+      })
     } else if (state.areaValue === "Bhaktapur") {
-      state.mapInstance.flyTo([27.672074636815736, 85.42961557359669], 15)
+      state.mapInstance.flyTo([27.672074636815736, 85.42961557359669], 13)
+      dispatch({
+        type: "changeMarkerPosition",
+        changeLatitude: 27.672074636815736,
+        changeLongitude: 85.42961557359669,
+      })
     }
   }, [state.areaValue])
+
+  //draggable marker
+  const markerRef = useRef(null)
+  const eventHandlers = useMemo(
+    () => ({
+      dragend() {
+        const marker = markerRef.current
+        console.log(marker.getLatLng())
+        dispatch({
+          type: "catchLatitudeChange",
+          latitudeChosen: marker.getLatLng().lat,
+        })
+        dispatch({
+          type: "catchLongitudeChange",
+          longitudeChosen: marker.getLatLng().lng,
+        })
+      },
+    }),
+    []
+  )
+
   return (
     <div className="flex flex-col items-center w-full mt-5">
       <h1 className="text-3xl my-6 font-semi">Add a Property</h1>
@@ -203,7 +277,6 @@ function AddProperty() {
             <option value={"Office"}>Office</option>
           </select>
         </div>
-
         <textarea
           className="h-32 px-3 py-2 shadow-md rounded-lg focus:outline-blue-300 "
           maxLength={200}
@@ -217,7 +290,10 @@ function AddProperty() {
             })
           }
         />
-
+        <h1 className="font-semibold text-sm text-gray-500">
+          Note: Latitude and Longitude can be chosen with the draggable marker
+          in the map.
+        </h1>
         <div className="flex gap-2">
           <input
             className="w-full h-10 px-3 shadow-md rounded-lg focus:outline-blue-300"
@@ -244,33 +320,23 @@ function AddProperty() {
             }
           />
         </div>
+        <h1 className="font-semibold text-sm text-gray-500">
+          Property Area in sqft & Price in Rs.
+        </h1>
         <div className="flex gap-2">
-          {state.propertyStatusValue === "Rent" ? (
-            <select
-              className="w-full h-10 px-3 shadow-md rounded-lg focus:outline-blue-300"
-              value={state.rentalFrequencyValue}
-              onChange={(e) =>
-                dispatch({
-                  type: "catchRentalFrequencyChange",
-                  rentalFrequencyChosen: e.target.value,
-                })
-              }
-            >
-              <option value="" selected hidden>
-                Select
-              </option>
-              <option value="Day">Day</option>
-              <option value="Week">Week</option>
-              <option value="Month">Month</option>
-            </select>
-          ) : (
-            <select
-              className="w-full h-10 px-3 shadow-md rounded-lg focus:outline-blue-300"
-              disabled
-            >
-              <option value="">Select</option>
-            </select>
-          )}
+          <input
+            className="w-full h-10 px-3 shadow-md rounded-lg focus:outline-blue-300"
+            type="text"
+            value={state.propertyAreaValue}
+            placeholder="PropertyArea"
+            onChange={(e) =>
+              dispatch({
+                type: "catchPropertyAreaChange",
+                propertyAreaChosen: e.target.value,
+              })
+            }
+          />
+
           <input
             className="w-full h-10 px-3 shadow-md rounded-lg focus:outline-blue-300"
             type="number"
@@ -284,7 +350,9 @@ function AddProperty() {
             }
           />
         </div>
-
+        <h1 className="font-semibold text-sm text-gray-500">
+          Mention number of Rooms and Parking space here.
+        </h1>
         <div className="flex gap-2">
           <input
             className="w-full h-10 px-3 shadow-md rounded-lg focus:outline-blue-300"
@@ -310,20 +378,54 @@ function AddProperty() {
               })
             }
           />
-          <input
+        </div>
+        <div className="flex gap-2">
+          <select
             className="w-full h-10 px-3 shadow-md rounded-lg focus:outline-blue-300"
             type="text"
-            value={state.propertyAreaValue}
-            placeholder="PropertyArea"
+            value={state.propertyStatusValue}
+            placeholder="Property Status"
             onChange={(e) =>
               dispatch({
-                type: "catchPropertyAreaChange",
-                propertyAreaChosen: e.target.value,
+                type: "catchPropertyStatusChange",
+                propertyStatusChosen: e.target.value,
               })
             }
-          />
-        </div>
+          >
+            <option selected hidden>
+              Select Property Status
+            </option>
+            <option value="Sale">Sale</option>
+            <option value="Rent">Rent</option>
+          </select>
 
+          {state.propertyStatusValue === "Rent" ? (
+            <select
+              className="w-full h-10 px-3 shadow-md rounded-lg focus:outline-blue-300"
+              value={state.rentalFrequencyValue}
+              onChange={(e) =>
+                dispatch({
+                  type: "catchRentalFrequencyChange",
+                  rentalFrequencyChosen: e.target.value,
+                })
+              }
+            >
+              <option value="" selected hidden>
+                Select Frequency
+              </option>
+              <option value="Day">Day</option>
+              <option value="Week">Week</option>
+              <option value="Month">Month</option>
+            </select>
+          ) : (
+            <select
+              className="w-full h-10 px-3 shadow-md rounded-lg focus:outline-blue-300"
+              disabled
+            >
+              <option value="">--</option>
+            </select>
+          )}
+        </div>
         <div className="flex gap-10 w-full items-center justify-center text-lg">
           <label htmlFor="furnished">
             <input
@@ -426,36 +528,88 @@ function AddProperty() {
               <option value="Kirtipur">Kirtipur</option>
             )}
           </select>
-          <select
-            className="w-full h-10 px-3 shadow-md rounded-lg focus:outline-blue-300"
-            type="text"
-            value={state.propertyStatusValue}
-            placeholder="Property Status"
-            onChange={(e) =>
-              dispatch({
-                type: "catchPropertyStatusChange",
-                propertyStatusChosen: e.target.value,
-              })
-            }
-          >
-            <option selected hidden>
-              Select Property Status
-            </option>
-            <option value="Sale">Sale</option>
-            <option value="Rent">Rent</option>
-          </select>
         </div>
         <div className="w-full h-[70vh]">
           <MapContainer
             center={[27.704111212111023, 85.31943175211019]}
-            zoom={15}
+            zoom={11}
             scrollWheelZoom={true}
           >
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
             <MapComponent />
+            {areaDisplay()}
+            <Marker
+              draggable
+              eventHandlers={eventHandlers}
+              position={state.markerPosition}
+              ref={markerRef}
+            ></Marker>
           </MapContainer>
         </div>
-        <button>Submit</button>
+        <div className="flex items-center justify-center w-full">
+          <label
+            htmlFor="dropzone-file"
+            className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 hover:bg-gray-100 "
+          >
+            <div className="flex flex-col items-center justify-center pt-5 pb-6">
+              <svg
+                aria-hidden="true"
+                className="w-10 h-10 mb-3 text-gray-400"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                />
+              </svg>
+              {state.uploadedImages.length === 0 ? (
+                <>
+                  <p className="mb-2 text-sm text-gray-500">
+                    <span className="font-semibold">Click to upload</span> or
+                    drag and drop
+                  </p>
+                  <p className="text-xs text-gray-500 ">
+                    PNG, JPG or GIF (MAX. 5 Pictures)
+                  </p>
+                </>
+              ) : (
+                <p className="mb-2 text-sm text-gray-500">
+                  <span className="font-semibold">{}</span>
+                </p>
+              )}
+            </div>
+            <input
+              id="dropzone-file"
+              type="file"
+              className="hidden"
+              multiple
+              accept="image/png,image/gif,image/jpeg"
+              onChange={(e) => {
+                dispatch({
+                  type: "catchUploadedImages",
+                  imagesChosen: e.target.files,
+                })
+              }}
+            />
+          </label>
+        </div>
+
+        <button
+          onClick={(e) => {
+            console.log(
+              Object.entries(state.uploadedImages).map((index, item) => (
+                <div>{item.name}</div>
+              ))
+            )
+          }}
+        >
+          Submit
+        </button>
       </form>
       {/* Map */}
     </div>
